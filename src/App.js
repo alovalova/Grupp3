4a8c2639a84c3981232d02c113ca5d280529c7e3
Amanda Tell <amanda@tengai-unbiased.com>
2021-05-15 11:46:39 +0200

    api call to create deck and draw card

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [deck, setDeck] = useState()
+  const [cards, setCards] = useState([])
+  const getDeck = async () => {
+    await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').then((res) =>
+      setDeck(res.data)
+    )
+  }
+
+  const drawCard = async () => {
+    await axios.get(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`).then((res) => {
+      setCards((cards) => [...cards, res.data.cards[0]])
+      document.querySelector('#currCard').src = res.data.cards[0].image
+    })
+  }
+
+  console.log(cards)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => getDeck()}>Click me to start</button>
+        <button onClick={() => drawCard()}>Click me to draw card</button>
+        <img src="#" id="currCard"></img>
       </header>
     </div>
   );
