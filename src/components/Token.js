81c36c359cf26e483b6e50baa608480e2fa9e1e3
alovalova <62546805+alovalova@users.noreply.github.com>
2021-05-29 21:25:55 +0200

    Update Token.js

--- src/components/Token.js
+++ src/components/Token.js
@@ -1,13 +1,14 @@
 import { useEffect, useState, useRef } from 'react'
 import token from './../img/token.png';
 
-export const Token = () => {
+export const Token = ({ winner }) => {
     if (!localStorage.getItem('Tokens'))
         localStorage.setItem('Tokens', 10)
     else if (!localStorage.getItem('Pot'))
         localStorage.setItem('Pot', 0)
-    else if (localStorage.getItem('Tokens') <= 0)
+    else if (localStorage.getItem('Tokens') <= 0 && localStorage.getItem('Pot') <= 0)
         alert("You are out of tokens.");
+
 
     const [tokens, setTokens] = useState(localStorage.getItem('Tokens'))
     const [potTokens, setPot] = useState(localStorage.getItem('Pot'))
@@ -22,18 +23,28 @@ export const Token = () => {
     const addToken = () => {
         setTokens(tokens - 1)
         localStorage.setItem('Tokens', tokens)
-
         setPot(+potTokens +1)
         localStorage.setItem('Pot', potTokens)
     }
-    /*
-    WinnerPot (() => {
-        if (winner === 'Player')
-            var income = localStorage.getItem('Pot')
-            var new_income = income.value * 2
-            console.log(new_income) 
-    })
-    */
+
+    useEffect(() => {
+        var income = parseInt(localStorage.getItem('Pot'))
+
+        if (winner === 'Player'){
+            localStorage.setItem('Pot', 0)
+            var old_income = parseInt(localStorage.getItem('Tokens'))
+            var new_income = income * 2
+            var total_income = new_income + old_income
+            localStorage.setItem('Tokens', total_income)
+            
+        } else if (winner === 'Dealer'){
+            localStorage.setItem('Pot', 0)
+            
+        } else if (winner === 'Draw'){
+            var draw_income = old_income + income
+            localStorage.setItem('Tokens', draw_income);
+        }
+    }, [winner])
 
     return (
         <div className="token">
