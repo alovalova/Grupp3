c8265181b746216b39759a03e43ed9a76b53c6e3
alovalova <62546805+alovalova@users.noreply.github.com>
2021-05-30 12:27:44 +0200

    Update card.js

--- src/modules/card.js
+++ src/modules/card.js
@@ -1,24 +1,7 @@
 
+
 export const sleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-export const getWinner = (dealerCards, playerCards) => {
-    let dealerTotal = 0
-    let playerTotal = 0
-    let winner
-    for (let i = 0; i < dealerCards.length; i++) {
-        dealerTotal += +getRealValue(dealerCards[i].value)
-    }
-    for (let i = 0; i < playerCards.length; i++) {
-        playerTotal += +getRealValue(playerCards[i].value)
-    }
-    if ((playerTotal > 21 && dealerTotal > 21) || playerTotal === dealerTotal) winner = 'Draw'
-    else if ((playerTotal < 21 && (playerTotal > dealerTotal)) || dealerTotal > 21) winner = 'Player'
-    else winner = 'Dealer'
-
-    console.log('dealer ', dealerTotal, 'player ', playerTotal);
-    return winner
 }
 
 export const getTotal = (total, dealerCards) => {
