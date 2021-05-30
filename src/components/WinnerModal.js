c834ee8565c7a5c3bfd85cfbabeeaa1de79ddcec
alovalova <62546805+alovalova@users.noreply.github.com>
2021-05-30 20:27:54 +0200

    Update WinnerModal.js

--- src/components/WinnerModal.js
+++ src/components/WinnerModal.js
@@ -2,7 +2,6 @@ import Modal from 'react-bootstrap/Modal'
 import { useEffect, useState } from 'react'
 
 export const WinnerModal = ({ winner, setWinner, totalCards}) => {
-    console.log(totalCards)
     const [show, setShow] = useState(false);
     useEffect(() => {
         if (winner !== '') setShow(true)
@@ -16,7 +15,7 @@ export const WinnerModal = ({ winner, setWinner, totalCards}) => {
     )
     return (
         <>
-            <Modal
+            <Modal className="modal"
                 show={show}
                 onHide={() => setShow(false)}
                 dialogClassName="modal-90w"
@@ -24,9 +23,7 @@ export const WinnerModal = ({ winner, setWinner, totalCards}) => {
             >
                 <Modal.Header closeButton>
                     <Modal.Title className='justify-content-center'>
-                    <h4>The winner is: {winner}</h4>
-                    <h5>Your total: {totalCards.playerTotal}</h5>
-                    <h5>Dealers total: {totalCards.dealerTotal}</h5>
+                    {totalCards !== "" ? (<div><h4>The winner is:</h4> <h2>{winner}</h2><h5>Your total: {totalCards.playerTotal}</h5><h5>Dealers total: {totalCards.dealerTotal}</h5></div>) : ("")} 
                     </Modal.Title>
                 </Modal.Header>
             </Modal>
