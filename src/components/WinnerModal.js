a4fd04c09099fbcaf6678ea25a238ca046d36cd2
Amanda Tell <amanda@tengai-unbiased.com>
2021-06-02 10:45:10 +0200

    add comments on WinnerModal.js

--- src/components/WinnerModal.js
+++ src/components/WinnerModal.js
@@ -1,13 +1,15 @@
 import Modal from 'react-bootstrap/Modal'
 import { useEffect, useState } from 'react'
 
-export const WinnerModal = ({ winner, setWinner, totalCards}) => {
-    const [show, setShow] = useState(false);
+export const WinnerModal = ({ winner, setWinner, totalCards }) => {
+    // Modal only shows when there is a winner
+    const [show, setShow] = useState(false)
     useEffect(() => {
         if (winner !== '') setShow(true)
         else setShow(false)
     }, [winner])
 
+    // Clear winner once modal closes
     useEffect(() => {
         if (show === false)
             setWinner("")
@@ -23,7 +25,7 @@ export const WinnerModal = ({ winner, setWinner, totalCards}) => {
             >
                 <Modal.Header closeButton>
                     <Modal.Title className='justify-content-center'>
-                    {totalCards !== "" ? (<div><h4>The winner is:</h4> <h2>{winner}</h2><h5>Your total: {totalCards.playerTotal}</h5><h5>Dealer total: {totalCards.dealerTotal}</h5></div>) : ("")} 
+                        {totalCards !== "" ? (<div><h4>The winner is:</h4> <h2>{winner}</h2><h5>Your total: {totalCards.playerTotal}</h5><h5>Dealer total: {totalCards.dealerTotal}</h5></div>) : ("")}
                     </Modal.Title>
                 </Modal.Header>
             </Modal>
